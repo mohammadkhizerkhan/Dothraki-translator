9eea1cc1a9a365f7662a8ff60f3fb659be969a61
mohammadkhizerkhan <64767673+mohammadkhizerkhan@users.noreply.github.com>
2021-06-09 23:37:23 +0530

    copied banana and changed to dhothraki

---
+++ app.js
@@ -0,0 +1,25 @@
+var btn = document.querySelector("#clickme");
+var txtIn = document.querySelector("#userinput");
+var output = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/dothraki.json";
+
+function translateURL(text) {
+  return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+  console.log("error occured", error);
+  alert("something wrong with server! try again after some time");
+}
+
+btn.addEventListener("click", function btnHandler() {
+  var inputTxt=txtIn.value
+  fetch(translateURL(inputTxt))
+    .then((response) => response.json())
+    .then((json) => {
+      output.innerText = json.contents.translated;
+    })
+    .catch(errorHandler);
+});
+
